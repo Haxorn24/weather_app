61ab99259940878faf22efcc986af04f29135f2a
Haxorn24 <76108964+Haxorn24@users.noreply.github.com>
2023-09-15 02:09:05 +0530

    Add files via upload

---
+++ src/forecast.js
@@ -0,0 +1,19 @@
+const key = "ptM1Z9ecL7PJQZXbsRDNsQpowl4onxXi";
+//get weather info
+const getweather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+//get city info
+
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
